70dbc986273736cd1bbbfb25cd163c2a1b831420
Patrick O'Boyle <oboylepa@tcd.ie>
2016-02-11 18:44:01 +0000

    - Added row,column positions to drones - Added getDistance method

--- src/hashcode2016/Drone.java
+++ src/hashcode2016/Drone.java
@@ -14,12 +14,18 @@ public class Drone {
     private int[] inventory;
     private int currentWeight;
     private static int carryingCapacity;
+    private int rowPos;
+    private int colPos;
+
     
-    public Drone(int idNumber, int numberOfItems, int capacity){
+    public Drone(int idNumber, int numberOfItems, int capacity, int startingRow, int startingCol){
         this.idNumber = idNumber;
         inventory = new int[numberOfItems];
         currentWeight = 0;
         carryingCapacity = capacity;
+        
+        this.rowPos = startingRow;
+        this.colPos = startingCol;
     }
     
     public boolean load(int numberOfItems, int itemType, int itemWeight){
@@ -37,5 +43,14 @@ public class Drone {
         }
         inventory[itemType]-=numberOfItems;
         return true;
+    }
+    
+    
+    // Returns int of distance to [rowB, colB]
+    public int getDistance(int rowB, int colB){
+        double form1 = (this.rowPos - rowB) * (this.rowPos - rowB);
+        double form2 = (this.colPos - colB) * (this.colPos - colB);
+        
+        return (int)Math.ceil( ( Math.sqrt(form1 + form2) ) );
     }
 }
